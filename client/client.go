8e938e85e0a4b75c9aea5d04a5049f51e8382a8b
Sangeeth <sangeethsajeev518@gmail.com>
2024-02-23 11:32:21 +0530

    rest api implememted

--- client/client.go
+++ client/client.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"context"
+	"net/http"
 
-	"github.com/sangeeth518/learning-grpc/proto"
+	"github.com/gin-gonic/gin"
+	proto "github.com/sangeeth518/learning-grpc/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+var client proto.ExampleClient
 
 func main() {
 	conn, err := grpc.Dial("localhost:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
 	}
-	client := proto.NewExampleClient(conn)
-	req := &proto.HelloRequest{SomeString: "Hello from client"}
-	client.ServerReply(context.TODO(), req)
+	client = proto.NewExampleClient(conn)
+	r := gin.Default()
+	r.GET("/sentmsg-to-server/:message", clientConnectionServer)
+	r.Run(":8000")
 
+}
+func clientConnectionServer(c *gin.Context) {
+	message := c.Param("message")
+	req := &proto.HelloRequest{SomeString: message}
+	client.ServerReply(context.TODO(), req)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "message sent succesfully to the server" + message,
+	})
 }
